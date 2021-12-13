3b9ca3f9deed0d4de2e94a47ca93975a6b39386c
Israel <idias@infofisco.com.br>
2021-12-13 08:50:41 -0300

    subindo dados

---
+++ WebApp/Startup.cs
@@ -0,0 +1,76 @@
+﻿using Hangfire;
+using Hangfire.PostgreSql;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            var connectionString = Configuration.GetConnectionString("DemoHangFireDB");
+            services.AddHangfire(optional => optional.UsePostgreSqlStorage(connectionString));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+            app.UseDeveloperExceptionPage();
+            app.UseDirectoryBrowser();
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            BackgroundJob.Enqueue(() => Console.WriteLine("Tarefa executada"));
+            BackgroundJob.Schedule(() => Console.WriteLine("Atraso na execução da tarefa"), TimeSpan.FromMinutes(1));
+
+            var id = BackgroundJob.Enqueue(() => Console.WriteLine("Hello, "));
+            BackgroundJob.ContinueWith(id, () => Console.WriteLine("world!"));
+        }
+    }
+}
